25149956a8a7d0548fa4d56ce164b57520f66871
cyber-yanus <yan.jb.hrib@mail.ru>
2020-06-21 16:20:15 +0300

    create Info scene

---
+++ Assets/Screens/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void loadNewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        Debug.Log("load " + sceneName + " scene");
+    }
+
+}
