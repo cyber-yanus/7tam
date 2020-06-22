5abfd4adfd6d34b4411f35cd56aa80d240d6167c
cyber-yanus <yan.jb.hrib@mail.ru>
2020-06-22 17:32:17 +0300

    create FloodFillArea algoritm

---
+++ Assets/Сoloring book/ColoringBookData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Coloring book/default coloting book", fileName ="New Coloring book")]
+public class ColoringBookData : ScriptableObject
+{
+    [SerializeField]
+    private Sprite coloringBook;
+    
+
+
+}
---
+++ Assets/Сoloring book/test_paint.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class test_paint : MonoBehaviour
+{
+    public Sprite sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        sprite.FloodFillArea(110, 100, Color.red);
+       // sprite.FloodFillBorder(400, 400, Color.red, Color.green);
+       
+        sprite.texture.Apply();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
