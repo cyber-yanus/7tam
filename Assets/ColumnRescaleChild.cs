cbfb1450a44be5ff3c7e88e8997a9d2ad72cb60a
cyber-yanus <yan.jb.hrib@mail.ru>
2020-06-24 01:10:13 +0300

    fixed rescale list in Main Scene

---
+++ Assets/ColumnRescaleChild.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColumnRescaleChild : MonoBehaviour
+{
+    public GameObject exampleParent;
+    public GameObject exampleChildrenContent;
+
+    RectTransform rectTransform;
+    GridLayoutGroup gridLayout;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        rectTransform = exampleParent.GetComponent<RectTransform>();
+        gridLayout = GetComponent<GridLayoutGroup>();
+
+        int childCount = transform.childCount;
+
+        float w = rectTransform.rect.width;
+        float h = rectTransform.rect.height / childCount;
+
+        childrenRescale(w, h);
+        lol(h);
+
+        Debug.Log("column height = " + h);
+        Debug.Log("column width = " + w);
+    }
+
+    private void childrenRescale(float w, float h)
+    {
+        gridLayout.cellSize = new Vector2(w, h);
+
+    }
+
+
+    private void lol(float h)
+    {
+        float childContentHeight = h - 40f;
+
+        gridLayout = exampleChildrenContent.GetComponent<GridLayoutGroup>();
+
+        gridLayout.cellSize = new Vector2(childContentHeight, childContentHeight);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LineRescaleChild.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LineRescaleChild : MonoBehaviour
+{
+    public GameObject exampleParent;
+
+    RectTransform rectTransform;
+    GridLayoutGroup gridLayout;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridLayout = GetComponent<GridLayoutGroup>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform = exampleParent.GetComponent<RectTransform>();
+
+        Rect rect = rectTransform.rect;
+
+        float h = rect.height;
+
+        childrenRescale(h);
+
+    }
+
+
+    private void childrenRescale(float h)
+    {
+        gridLayout.cellSize = new Vector2(h, h);
+    }
+}
