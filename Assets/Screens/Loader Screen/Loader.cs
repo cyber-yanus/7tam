65fe9af91288f0572c8b82b6ad1e9e095412782e
cyber-yanus <yan.jb.hrib@mail.ru>
2020-06-29 03:42:20 +0300

    add ADS

--- Assets/Screens/Loader Screen/Loader.cs
+++ Assets/Screens/Loader Screen/Loader.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class Loader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        if (Advertisement.isSupported)
+            Advertisement.Initialize("3682983", true);
+
         StartCoroutine("waitSimulator");
     }
 
     IEnumerator waitSimulator()
     {
-        yield return new WaitForSeconds(1.5f);
-        SceneLoader.AsyncLoadNewScene("Main");
+        yield return new WaitForSeconds(1f);
+        
+        SceneLoader.AsyncLoadNewScene("Main");  
     }
 
 }
---
+++ Assets/Screens/Shop Screen/scripts/WatchButtonListner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class WatchButtonListner : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(execute);
+    }
+
+    private void execute()
+    {
+        if (Advertisement.IsReady())
+            Advertisement.Show("video");
+    }
+
+}
